00e03118bce81c6a3e09053e45045fb6a9a42785
John Bandela <jbandela@gmail.com>
2016-04-09 18:54:37 -0400

    Added Boost License

--- test.cpp
+++ test.cpp
@@ -1,3 +1,8 @@
+// Copyright 2016 John R. Bandela
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include "stackless_coroutine.hpp"
 #include <thread>
 #define CATCH_CONFIG_MAIN
